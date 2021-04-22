6791d1f9f0fc3eeb35f6db1eeabb3d42768427e9
r-m-hufford <ryan@rmhufford.com>
2021-04-22 17:06:16 -0400

    complete

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,71 +1,59 @@
 /* poll 1 */
-
 INSERT INTO poll(poll_id, question) VALUES (1, 'what is your favorite color?');
 INSERT INTO option (option_id, option_value, poll_id) values (1, 'Red', 1), (2, 'Blue', 1), (3, 'Green', 1);
 
 /* poll 2 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'what is your favorite food?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'burritos', 2), (2, 'burgers', 2), (3, 'nachos', 2);
+INSERT INTO poll(poll_id, question) VALUES (2, 'what is your favorite food?');
+INSERT INTO option (option_id, option_value, poll_id) values (4, 'burritos', 2), (5, 'burgers', 2), (6, 'nachos', 2);
 
 /* poll 3 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'what is your favorite color?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'Red', 3), (2, 'Blue', 3), (3, 'Green', 3);
+INSERT INTO poll(poll_id, question) VALUES (3, 'what is your favorite color?');
+INSERT INTO option (option_id, option_value, poll_id) values (7, 'Red', 3), (8, 'Blue', 3), (9, 'Green', 3);
 
 /* poll 4 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'who is your favorite continental philosopher?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'Nietzche', 4), (2, 'Marcuse', 4), (3, 'Debord', 4);
+INSERT INTO poll(poll_id, question) VALUES (4, 'who is your favorite continental philosopher?');
+INSERT INTO option (option_id, option_value, poll_id) values (10, 'Nietzche', 4), (11, 'Marcuse', 4), (12, 'Debord', 4);
 
 /* poll 5 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'who is your favorite analytical philosopher?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'Russel', 5), (2, 'Hume', 5), (3, 'Popper', 5);
+INSERT INTO poll(poll_id, question) VALUES (5, 'who is your favorite analytical philosopher?');
+INSERT INTO option (option_id, option_value, poll_id) values (13, 'Russel', 5), (14, 'Hume', 5), (15, 'Popper', 5);
 
 /* poll 6 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'what is your favorite book?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'White noise', 6), (2, 'slaughterhouse five', 6), (3, 'no country for old men', 6);
+INSERT INTO poll(poll_id, question) VALUES (6, 'what is your favorite book?');
+INSERT INTO option (option_id, option_value, poll_id) values (16, 'White noise', 6), (17, 'slaughterhouse five', 6), (18, 'no country for old men', 6);
 
 /* poll 7 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'how many years until humans annihilate themselves?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, '10', 7), (2, '100', 7), (3, '1000', 7);
+INSERT INTO poll(poll_id, question) VALUES (7, 'how many years until humans annihilate themselves?');
+INSERT INTO option (option_id, option_value, poll_id) values (19, '10', 7), (20, '100', 7), (21, '1000', 7);
 
 /* poll 8 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'what is your favorite programming language?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'python', 8), (2, 'java', 8), (3, 'c++', 8);
+INSERT INTO poll(poll_id, question) VALUES (8, 'what is your favorite programming language?');
+INSERT INTO option (option_id, option_value, poll_id) values (22, 'python', 8), (23, 'java', 8), (24, 'c++', 8);
 
 /* poll 9 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'could jack have fit on the door with rose?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'yes', 9), (2, 'no', 9), (3, 'maybe', 9);
+INSERT INTO poll(poll_id, question) VALUES (9, 'could jack have fit on the door with rose?');
+INSERT INTO option (option_id, option_value, poll_id) values (25, 'yes', 9), (26, 'no', 9), (27, 'maybe', 9);
 
 /* poll 10 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'how likely are you to take this poll?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'likely', 10), (2, 'neutral', 10), (3, 'unsure', 10);
+INSERT INTO poll(poll_id, question) VALUES (10, 'how likely are you to take this poll?');
+INSERT INTO option (option_id, option_value, poll_id) values (28, 'likely', 10), (29, 'neutral', 10), (30, 'unlikely', 10);
 
 /* poll 11 */
+INSERT INTO poll(poll_id, question) VALUES (11, 'what is your favorite kind of tax?');
+INSERT INTO option (option_id, option_value, poll_id) values (31, 'income', 11), (32, 'sales', 11), (33, 'gas', 11);
 
-INSERT INTO poll(poll_id, question) VALUES (1, 'what is your favorite kind of tax?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'income', 11), (2, 'sales', 11), (3, 'gas', 11);
 /* poll 12 */
+INSERT INTO poll(poll_id, question) VALUES (12, 'what is your favorite snake?');
+INSERT INTO option (option_id, option_value, poll_id) values (34, 'python', 12), (35, 'cobra', 12), (36, 'rattler', 12);
 
-INSERT INTO poll(poll_id, question) VALUES (1, 'what is your favorite snake?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'python', 12), (2, 'cobra', 12), (3, 'rattler', 12);
 /* poll 13 */
+INSERT INTO poll(poll_id, question) VALUES (13, 'are you right or left handed?');
+INSERT INTO option (option_id, option_value, poll_id) values (37, 'right', 13), (38, 'left', 13), (39, 'ambidextrous', 13);
 
-INSERT INTO poll(poll_id, question) VALUES (1, 'are you right or left handed?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'right', 13), (2, 'left', 13), (3, 'ambidextrous', 13);
 /* poll 14 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'who is your favorite impressionist painter?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'cezanne', 14), (2, 'renoit', 14), (3, 'monet', 14);
+INSERT INTO poll(poll_id, question) VALUES (14, 'who is your favorite impressionist painter?');
+INSERT INTO option (option_id, option_value, poll_id) values (40, 'cezanne', 14), (41, 'renoit', 14), (42, 'monet', 14);
 
 /* poll 15 */
-
-INSERT INTO poll(poll_id, question) VALUES (1, 'how much wood would a woodchuck chuck if a woodchuck could chuck wood?');
-INSERT INTO option (option_id, option_value, poll_id) values (1, 'a reasonable amount', 15), (2, 'An excessive amount', 15), (3, 'none', 15);
+INSERT INTO poll(poll_id, question) VALUES (15, 'how much wood would a woodchuck chuck if a woodchuck could chuck wood?');
+INSERT INTO option (option_id, option_value, poll_id) values (43, 'a reasonable amount', 15), (44, 'An excessive amount', 15), (45, 'none', 15);
